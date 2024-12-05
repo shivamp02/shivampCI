5b69949cc9287e435958deeb4693421aacdbb2a0
shivamp02 <shivampatel@cpp.edu>
2024-12-05 14:23:40 -0800

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,7 +2,7 @@ echo "Building the sample project..."
 # Install dependencies
 
 # Run the Python script to check if it works
-python3 sample_code.py
+python3 jenkins/sample_code.py
 
 # Check for PEP 8 compliance
 echo "Checking for PEP 8 compliance..."
